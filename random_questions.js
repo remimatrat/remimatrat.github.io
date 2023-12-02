1832fb5d6f782c14034d255b908a60175b84e376
remidoobi <remi.matrat@gmail.com>
2023-12-02 10:28:14 +0100

    Update random_questions.js

--- random_questions.js
+++ random_questions.js
@@ -30,7 +30,7 @@ var quoteArray = [
   { quote: "Do you have a specific practice to peel fruits?" },
   { quote: "Are you a morning person?" },
   { quote: "Blue pill or red pill?\r\n'You take the blue pill—the story ends, you wake up in your bed and believe whatever you want to believe. You take the red pill—you stay in Wonderland, and I show you how deep the rabbit hole goes. Remember: all I'm offering is the truth. Nothing more.'" },
-  { quote: "Did you ever made a trip only by yourself? \n Where was it? \n How long did it last?" },
+  { quote: "Did you ever made a trip only by yourself? \r\n Where was it? \r\n How long did it last?" },
   { quote: "What is the favortie picture of you when your were very young?" },
   { quote: "We all did stupid things. What is the stupidest things you did?" },
   { quote: "According to you, what was the most decisive moment of your life?" },
